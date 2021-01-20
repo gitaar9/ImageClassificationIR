cbf5683d71a858d013005ae5393eb441c056f053
gitaar9 <thijs.eker@gmail.com>
2021-01-20 16:59:48 +0100

    backup my progress with splitting dataset

--- ir_image_classification/data_cleanup/split_dataset.py
+++ ir_image_classification/data_cleanup/split_dataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 import cv2
 from keras_preprocessing.image import ImageDataGenerator
 
-from ir_image_classification.feature_extraction.keras.keras_dataset import marvel_dataframe, classes
+from ir_image_classification.feature_extraction.keras.keras_dataset import marvel_dataframe, classes, df_to_histogram
 import numpy as np
 
 
@@ -28,7 +28,7 @@ already_checked_paths = set(side_view_images['paths']).union(set(other_view_imag
 )
 
 # Load the data from the standard marvel dataset
-allowed_classes = {0, 5, 8, 9, 17, 19, 21, 22}
+allowed_classes = {17}  # {0, 5, 8, 9, 17, 19, 21, 22}
 marvel_root_dir = '/home/gitaar9/AI/TNO/marveldataset2016/'
 train_df = marvel_dataframe(marvel_root_dir, is_train=True, cast_labels_to=int)
 train_df = train_df[train_df['labels'].isin(allowed_classes)]  # Remove not allowed classes
@@ -37,39 +37,51 @@ train_df = train_df.sample(frac=1)  # Shuffle the df
 
 
 # Loop for splitting the images in sideview or otherview datasets
-start_time = time.time()
-total_images_added = 0.1
-cv2.namedWindow("Split images")
-for (_, image_path, label) in train_df.itertuples(name=None):
-    print("Images per minute: {:.2f}".format(60 / ((time.time() - start_time) / total_images_added)))
-    print(f"\nClass is {classes[label]}. 0(Sideview), 1(Otherview), 2(Skip), 3(Quit program)")
-    print(image_path, label)
+try:
+    start_time = time.time()
+    total_images_added = 0.1
+    cv2.namedWindow("Split images")
+    for (_, image_path, label) in train_df.itertuples(name=None):
+        print("Images per minute: {:.2f}".format(60 / ((time.time() - start_time) / total_images_added)))
+        print(f"\nClass is {classes[label]}. 0(Sideview), 1(Otherview), 2(Skip), 3(Quit program)")
+        print(image_path, label)
 
-    image = cv2.imread(image_path)  # Load the image
-    cv2.imshow("Split images", image.astype(np.uint8))  # Show the image
-    k = chr(cv2.waitKey(100000))  # Wait for keyboard input
+        image = cv2.imread(image_path)  # Load the image
+        cv2.imshow("Split images", image.astype(np.uint8))  # Show the image
+        k = chr(cv2.waitKey(100000))  # Wait for keyboard input
 
-    if k == '0':  # toggle current image
-        total_images_added += 1
-        print('Add to sideview dataset')
-        side_view_images = side_view_images.append({'paths': image_path, 'labels': label}, ignore_index=True)
-        continue
-    elif k == '1':
-        total_images_added += 1
-        print('Add to otherview dataset')
-        other_view_images = other_view_images.append({'paths': image_path, 'labels': label}, ignore_index=True)
-        continue
-    elif k == '2':
-        print('Skipped image')
-        skipped_images = skipped_images.append({'paths': image_path, 'labels': label}, ignore_index=True)
-        continue
-    elif k == '3':
-        print('Exiting...')
-        break
+        if k == '0':  # toggle current image
+            total_images_added += 1
+            print('Add to sideview dataset')
+            side_view_images = side_view_images.append({'paths': image_path, 'labels': label}, ignore_index=True)
+            continue
+        elif k == '1':
+            total_images_added += 1
+            print('Add to otherview dataset')
+            other_view_images = other_view_images.append({'paths': image_path, 'labels': label}, ignore_index=True)
+            continue
+        elif k == '2':
+            print('Skipped image')
+            skipped_images = skipped_images.append({'paths': image_path, 'labels': label}, ignore_index=True)
+            continue
+        elif k == '3':
+            print('Exiting...')
+            break
+except ValueError:
+    print('Time ran out closing the program')
+except:
+    print('Unkown error, saving to backup files')
+    side_view_images.to_hdf('side_view_images_error.hdf', 'df', mode='w')
+    other_view_images.to_hdf('other_view_images_error.hdf', 'df', mode='w')
+    skipped_images.to_hdf('skipped_images.hdf_error', 'df', mode='w')
+    exit()
 
 # Save the dataframes in the end as well
 side_view_images.to_hdf('side_view_images.hdf', 'df', mode='w')
 other_view_images.to_hdf('other_view_images.hdf', 'df', mode='w')
 skipped_images.to_hdf('skipped_images.hdf', 'df', mode='w')
+
+print("Side view:", df_to_histogram(side_view_images))
+print("Other view:", df_to_histogram(other_view_images))
 
 cv2.destroyAllWindows()
